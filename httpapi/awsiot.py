9fbf1efd8f44251b2213395cf0b5dc2b6a32861f
RUTE-FSMUSEUM <contact@fsmuseum.jpn.org>
2025-05-25 02:51:01 +0900

    AWS IoT Coreと連携する機能追加

---
+++ httpapi/awsiot.py
@@ -0,0 +1,138 @@
+import os
+import yaml
+import urllib.request
+# Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+# SPDX-License-Identifier: Apache-2.0.
+
+from awscrt import mqtt
+from awsiot import mqtt_connection_builder
+import sys
+import time
+from utils.command_line_utils import CommandLineUtils
+
+HTTPAPI_PATH = os.path.dirname(os.path.abspath(__file__))
+ROOT_PATH = os.path.dirname(HTTPAPI_PATH)
+CONFIG_PATH = os.path.join(HTTPAPI_PATH, 'server.config.yaml')
+
+with open(CONFIG_PATH, 'r') as yml:
+    config = yaml.safe_load(yml)
+
+# This sample uses the Message Broker for AWS IoT to send and receive messages
+# through an MQTT connection. On startup, the device connects to the server,
+# subscribes to a topic, and begins publishing messages to that topic.
+# The device should receive those same messages back from the message broker,
+# since it is subscribed to that same topic.
+
+# cmdData is the arguments/input from the command line placed into a single struct for
+# use in this sample. This handles all of the command line parsing, validating, etc.
+# See the Utils/CommandLineUtils for more information.
+cmdData = CommandLineUtils.parse_sample_input_pubsub()
+
+# Callback when connection is accidentally lost.
+def on_connection_interrupted(connection, error, **kwargs):
+    print("Connection interrupted. error: {}".format(error))
+
+
+# Callback when an interrupted connection is re-established.
+def on_connection_resumed(connection, return_code, session_present, **kwargs):
+    print("Connection resumed. return_code: {} session_present: {}".format(return_code, session_present))
+
+    if return_code == mqtt.ConnectReturnCode.ACCEPTED and not session_present:
+        print("Session did not persist. Resubscribing to existing topics...")
+        resubscribe_future, _ = connection.resubscribe_existing_topics()
+
+        # Cannot synchronously wait for resubscribe result because we're on the connection's event-loop thread,
+        # evaluate result with a callback instead.
+        resubscribe_future.add_done_callback(on_resubscribe_complete)
+
+
+def on_resubscribe_complete(resubscribe_future):
+    resubscribe_results = resubscribe_future.result()
+    print("Resubscribe results: {}".format(resubscribe_results))
+
+    for topic, qos in resubscribe_results['topics']:
+        if qos is None:
+            sys.exit("Server rejected resubscribe to topic: {}".format(topic))
+
+
+# Callback when the subscribed topic receives a message
+def on_message_received(topic, payload, dup, qos, retain, **kwargs):
+    print("Received message from topic '{}': {}".format(topic, payload))
+    payload = (payload.decode('utf-8')).split(":")
+    if len(payload) == 2:
+        req = urllib.request.Request(f"http://{config['SERVER']['HOST']}:{config['SERVER']['PORT']}/api?app={payload[0]}&cmd={payload[1]}")
+        with urllib.request.urlopen(req) as res:
+            body = res.read()
+        print(body)
+
+# Callback when the connection successfully connects
+def on_connection_success(connection, callback_data):
+    assert isinstance(callback_data, mqtt.OnConnectionSuccessData)
+    print("Connection Successful with return code: {} session present: {}".format(callback_data.return_code, callback_data.session_present))
+
+# Callback when a connection attempt fails
+def on_connection_failure(connection, callback_data):
+    assert isinstance(callback_data, mqtt.OnConnectionFailureData)
+    print("Connection failed with error code: {}".format(callback_data.error))
+
+# Callback when a connection has been disconnected or shutdown successfully
+def on_connection_closed(connection, callback_data):
+    print("Connection closed")
+
+if __name__ == '__main__':
+    # Create a MQTT connection from the command line data
+    mqtt_connection = mqtt_connection_builder.mtls_from_path(
+        endpoint=cmdData.input_endpoint,
+        port=cmdData.input_port,
+        cert_filepath=cmdData.input_cert,
+        pri_key_filepath=cmdData.input_key,
+        ca_filepath=cmdData.input_ca,
+        on_connection_interrupted=on_connection_interrupted,
+        on_connection_resumed=on_connection_resumed,
+        client_id=cmdData.input_clientId,
+        clean_session=False,
+        keep_alive_secs=30,
+        http_proxy_options=None,
+        on_connection_success=on_connection_success,
+        on_connection_failure=on_connection_failure,
+        on_connection_closed=on_connection_closed)
+
+    if not cmdData.input_is_ci:
+        print(f"Connecting to {cmdData.input_endpoint} with client ID '{cmdData.input_clientId}'...")
+    else:
+        print("Connecting to endpoint with client ID")
+    connect_future = mqtt_connection.connect()
+
+    # Future.result() waits until a result is available
+    connect_future.result()
+    print("Connected!")
+
+    message_topic = cmdData.input_topic
+    message_string = cmdData.input_message
+
+    # Subscribe
+    print("Subscribing to topic '{}'...".format(message_topic))
+    subscribe_future, packet_id = mqtt_connection.subscribe(
+        topic=message_topic,
+        qos=mqtt.QoS.AT_LEAST_ONCE,
+        callback=on_message_received)
+
+    subscribe_result = subscribe_future.result()
+    print("Subscribed with {}".format(str(subscribe_result['qos'])))
+
+    # Publish message to server desired number of times.
+    # This step is skipped if message is blank.
+    # This step loops forever if count was set to 0.
+    if message_string:
+        print("Receiving messages until program killed")
+        try:
+            while True:
+                time.sleep(1)
+        except KeyboardInterrupt:
+            pass
+
+    # Disconnect
+    print("Disconnecting...")
+    disconnect_future = mqtt_connection.disconnect()
+    disconnect_future.result()
+    print("Disconnected!")
---
+++ httpapi/utils/command_line_utils.py
@@ -0,0 +1,210 @@
+# Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+# SPDX-License-Identifier: Apache-2.0.
+
+import argparse
+from awscrt import io
+from uuid import uuid4
+
+class CommandLineUtils:
+    def __init__(self, description) -> None:
+        self.parser = argparse.ArgumentParser(description="Send and receive messages through and MQTT connection.")
+        self.commands = {}
+        self.parsed_commands = None
+
+    def register_command(self, command_name, example_input, help_output, required=False, type=None, default=None, choices=None, action=None):
+        self.commands[command_name] = {
+            "name":command_name,
+            "example_input":example_input,
+            "help_output":help_output,
+            "required": required,
+            "type": type,
+            "default": default,
+            "choices": choices,
+            "action": action
+        }
+
+    def remove_command(self, command_name):
+        if command_name in self.commands.keys():
+            self.commands.pop(command_name)
+
+    """
+    Returns the command if it exists and has been passed to the console, otherwise it will print the help for the sample and exit the application.
+    """
+    def get_command_required(self, command_name, command_name_alt = None):
+        if(command_name_alt != None):
+            if hasattr(self.parsed_commands, command_name_alt):
+                if(getattr(self.parsed_commands, command_name_alt) != None):
+                    return getattr(self.parsed_commands, command_name_alt)
+
+        if hasattr(self.parsed_commands, command_name):
+            if(getattr(self.parsed_commands, command_name) != None):
+                return getattr(self.parsed_commands, command_name)
+
+        self.parser.print_help()
+        print("Command --" + command_name + " required.")
+        exit()
+
+    """
+    Returns the command if it exists, has been passed to the console, and is not None. Otherwise it returns whatever is passed as the default.
+    """
+    def get_command(self, command_name, default=None):
+        if hasattr(self.parsed_commands, command_name):
+            result = getattr(self.parsed_commands, command_name)
+            if (result != None):
+                return result
+        return default
+
+    def get_args(self):
+        # if we have already parsed, then return the cached parsed commands
+        if self.parsed_commands is not None:
+            return self.parsed_commands
+
+        # add all the commands
+        for command in self.commands.values():
+            if not command["action"] is None:
+                self.parser.add_argument("--" + command["name"], action=command["action"], help=command["help_output"],
+                    required=command["required"], default=command["default"])
+            else:
+                self.parser.add_argument("--" + command["name"], metavar=command["example_input"], help=command["help_output"],
+                    required=command["required"], type=command["type"], default=command["default"], choices=command["choices"])
+
+        self.parsed_commands = self.parser.parse_args()
+        # Automatically start logging if it is set
+        if self.parsed_commands.verbosity:
+            io.init_logging(getattr(io.LogLevel, self.parsed_commands.verbosity), 'stderr')
+
+        return self.parsed_commands
+    
+    def add_common_mqtt_commands(self):
+        self.register_command(
+            CommandLineUtils.m_cmd_endpoint,
+            "<str>",
+            "The endpoint of the mqtt server not including a port.",
+            True,
+            str)
+        self.register_command(
+            CommandLineUtils.m_cmd_ca_file,
+            "<path>",
+            "Path to AmazonRootCA1.pem (optional, system trust store used by default)",
+            False,
+            str)
+        self.register_command(
+            CommandLineUtils.m_cmd_is_ci,
+            "<str>",
+            "If present the sample will run in CI mode (optional, default='None')",
+            False,
+            str)
+        
+    def add_common_proxy_commands(self):
+        self.register_command(
+            CommandLineUtils.m_cmd_proxy_host,
+            "<str>",
+            "Host name of the proxy server to connect through (optional)",
+            False,
+            str)
+        self.register_command(
+            CommandLineUtils.m_cmd_proxy_port,
+            "<int>",
+            "Port of the http proxy to use (optional, default='8080')",
+            type=int,
+            default=8080)
+
+    def add_common_topic_message_commands(self):
+        self.register_command(
+            CommandLineUtils.m_cmd_topic,
+            "<str>",
+            "Topic to publish, subscribe to (optional, default='test/topic').",
+            default="test/topic")
+        self.register_command(
+            CommandLineUtils.m_cmd_message,
+            "<str>",
+            "The message to send in the payload (optional, default='Hello World!').",
+            default="Hello World! ")
+
+    def add_common_logging_commands(self):
+        self.register_command(
+            CommandLineUtils.m_cmd_verbosity,
+            "<Log Level>",
+            "Logging level.",
+            default=io.LogLevel.NoLogs.name,
+            choices=[
+                x.name for x in io.LogLevel])
+
+    def add_common_key_cert_commands(self):
+        self.register_command(CommandLineUtils.m_cmd_key_file, "<path>", "Path to your key in PEM format.", True, str)
+        self.register_command(CommandLineUtils.m_cmd_cert_file, "<path>", "Path to your client certificate in PEM format.", True, str)
+
+
+    ########################################################################
+    # cmdData utils/functions
+    ########################################################################
+
+    class CmdData:
+        # General use
+        input_endpoint : str
+        input_cert : str
+        input_key : str
+        input_ca : str
+        input_clientId : str
+        input_port : int
+        input_is_ci : bool
+        input_use_websockets : bool
+        # Proxy
+        input_proxy_host : str
+        input_proxy_port : int
+        # PubSub
+        input_topic : str
+        input_message : str
+        input_count : int
+
+        def __init__(self) -> None:
+            pass
+
+        def parse_input_topic(self, cmdUtils):
+            self.input_topic = cmdUtils.get_command(CommandLineUtils.m_cmd_topic, "test/topic")
+            if (cmdUtils.get_command(CommandLineUtils.m_cmd_is_ci) != None):
+                self.input_topic += "/" + str(uuid4())
+
+
+    def parse_sample_input_pubsub():
+        cmdUtils = CommandLineUtils("PubSub - Send and receive messages through an MQTT connection.")
+        cmdUtils.add_common_mqtt_commands()
+        cmdUtils.add_common_topic_message_commands()
+        cmdUtils.add_common_proxy_commands()
+        cmdUtils.add_common_logging_commands()
+        cmdUtils.add_common_key_cert_commands()
+        cmdUtils.register_command(CommandLineUtils.m_cmd_port, "<int>", "Connection port. AWS IoT supports 443 and 8883 (optional, default=8883).", type=int)
+        cmdUtils.register_command(CommandLineUtils.m_cmd_client_id, "<str>", "Client ID to use for MQTT connection (optional, default='test-*').", default="test-" + str(uuid4()))
+        cmdUtils.register_command(CommandLineUtils.m_cmd_count, "<int>", "The number of messages to send (optional, default='0').", default=0, type=int)
+        cmdUtils.get_args()
+
+        cmdData = CommandLineUtils.CmdData()
+        cmdData.input_endpoint = cmdUtils.get_command_required(CommandLineUtils.m_cmd_endpoint)
+        cmdData.input_port = int(cmdUtils.get_command(CommandLineUtils.m_cmd_port, 8883))
+        cmdData.input_cert = cmdUtils.get_command_required(CommandLineUtils.m_cmd_cert_file)
+        cmdData.input_key = cmdUtils.get_command_required(CommandLineUtils.m_cmd_key_file)
+        cmdData.input_ca = cmdUtils.get_command(CommandLineUtils.m_cmd_ca_file, None)
+        cmdData.input_clientId = cmdUtils.get_command(CommandLineUtils.m_cmd_client_id, "test-" + str(uuid4()))
+        cmdData.input_proxy_host = cmdUtils.get_command(CommandLineUtils.m_cmd_proxy_host)
+        cmdData.input_proxy_port = int(cmdUtils.get_command(CommandLineUtils.m_cmd_proxy_port))
+        cmdData.input_message = cmdUtils.get_command(CommandLineUtils.m_cmd_message, "Hello World! ")
+        cmdData.parse_input_topic(cmdUtils)
+        cmdData.input_count = int(cmdUtils.get_command(CommandLineUtils.m_cmd_count, 10))
+        cmdData.input_is_ci = cmdUtils.get_command(CommandLineUtils.m_cmd_is_ci, None) != None
+        return cmdData
+    
+
+    # Constants for commonly used/needed commands
+    m_cmd_endpoint = "endpoint"
+    m_cmd_ca_file = "ca_file"
+    m_cmd_cert_file = "cert"
+    m_cmd_key_file = "key"
+    m_cmd_proxy_host = "proxy_host"
+    m_cmd_proxy_port = "proxy_port"
+    m_cmd_message = "message"
+    m_cmd_topic = "topic"
+    m_cmd_verbosity = "verbosity"
+    m_cmd_port = "port"
+    m_cmd_client_id = "client_id"
+    m_cmd_is_ci = "is_ci"
+    m_cmd_count = "count"
